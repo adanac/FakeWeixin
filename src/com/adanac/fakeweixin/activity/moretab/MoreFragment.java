2527ca03c5e21d79b3208c51a6cbf973528891da
adanac <adanac@sina.com>
2015-12-16 21:53:07 +0800

    add listerevent

--- src/com/adanac/fakeweixin/activity/moretab/MoreFragment.java
+++ src/com/adanac/fakeweixin/activity/moretab/MoreFragment.java
@@ -27,8 +27,16 @@ public class MoreFragment extends LazyFragment {
 		tabIndex = getArguments().getInt(INTENT_INT_INDEX);
 		progressBar = (ProgressBar) findViewById(R.id.fragment_mainTab_item_progressBar);
 		textView = (TextView) findViewById(R.id.fragment_mainTab_item_textView);
-		textView.setText("界面" + " " + tabIndex + " 加载完毕");
-		handler.sendEmptyMessageDelayed(1, 2000);
+		switch (tabIndex) {
+		case 0:
+			textView.setText("媒介就是承载信息的一切中介物。而媒介素养则是面对各种媒介信息时的选择能力、理解能力、质疑能力、评价能力、应用能力。  媒介素养可以成为帮助我们理解媒介文化的“新眼镜”。通过对具体媒介作品的解读训练，可以在感知、体验、沟通与交流的过程中逐步唤醒自觉意识，增强评价能力，为今后的媒介表达奠定扎实基础。");
+			break;
+
+		default:
+			textView.setText("界面 " + tabIndex + " 加载完毕");
+			break;
+		}
+		handler.sendEmptyMessageDelayed(1, 1000);
 	}
 
 	@Override
--- src/com/adanac/fakeweixin/activity/moretab/MoreTabMJSYActivity.java
+++ src/com/adanac/fakeweixin/activity/moretab/MoreTabMJSYActivity.java
@@ -8,9 +8,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.adanac.fakeweixin.R;
@@ -29,8 +32,32 @@ import com.adanac.view.indicator.transition.OnTransitionTextListener;
 public class MoreTabMJSYActivity extends FragmentActivity {
 	private IndicatorViewPager indicatorViewPager;
 	private LayoutInflater inflate;
-	private String[] names = { "知识点注释", "知识地图", "习题作业", "作业讨论", "相关资源" };
+	private String[] names = { "知识点注释", "知识地图", "习题作业", "相关资源" };
 	private ScrollIndicatorView indicator;
+
+	// TODO
+	// 底部菜单没有显示出来是因为代码中没有写出来
+
+	// 底部菜单4个Linearlayout
+	private LinearLayout ll_home;
+	private LinearLayout ll_address;
+	private LinearLayout ll_friend;
+	private LinearLayout ll_setting;
+
+	// 底部菜单4个ImageView
+	private ImageView iv_home;
+	private ImageView iv_address;
+	private ImageView iv_friend;
+	private ImageView iv_setting;
+
+	// 顶部标题
+	private TextView moretab_mjsy_top;
+
+	// 底部菜单4个菜单标题
+	private TextView tv_home;
+	private TextView tv_address;
+	private TextView tv_friend;
+	private TextView tv_setting;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +67,12 @@ public class MoreTabMJSYActivity extends FragmentActivity {
 		ViewPager viewPager = (ViewPager) findViewById(R.id.moretab_viewPager);
 		indicator = (ScrollIndicatorView) findViewById(R.id.moretab_indicator);
 		indicator.setScrollBar(new ColorBar(this, Color.RED, 5));
+
+		// 画出底部菜单
+		initBottomView();
+
+		// 设置监听
+		initEvent();
 
 		// 设置滚动监听
 		int selectColorId = R.color.tab_top_text_2;
@@ -55,6 +88,48 @@ public class MoreTabMJSYActivity extends FragmentActivity {
 
 	}
 
+	private void initEvent() {
+		// 设置按钮监听
+		ll_home.setOnClickListener((OnClickListener) this);
+		ll_address.setOnClickListener((OnClickListener) this);
+		ll_friend.setOnClickListener((OnClickListener) this);
+		ll_setting.setOnClickListener((OnClickListener) this);
+
+		// 设置返回按钮
+		findViewById(R.id.iv_back).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						// 使当前Activity结束就可以了
+						MoreTabMJSYActivity.this.finish();
+					}
+				});
+	}
+
+	private void initBottomView() {
+		// 底部菜单4个Linearlayout
+		this.ll_home = (LinearLayout) findViewById(R.id.ll_home);
+		this.ll_address = (LinearLayout) findViewById(R.id.ll_address);
+		this.ll_friend = (LinearLayout) findViewById(R.id.ll_friend);
+		this.ll_setting = (LinearLayout) findViewById(R.id.ll_setting);
+
+		// 底部菜单4个ImageView
+		this.iv_home = (ImageView) findViewById(R.id.iv_home);
+		this.iv_address = (ImageView) findViewById(R.id.iv_address);
+		this.iv_friend = (ImageView) findViewById(R.id.iv_friend);
+		this.iv_setting = (ImageView) findViewById(R.id.iv_setting);
+
+		// 顶部标题
+		this.moretab_mjsy_top = (TextView) findViewById(R.id.moretab_mjsy_top);
+
+		// 底部菜单4个菜单标题
+		this.tv_home = (TextView) findViewById(R.id.tv_home);
+		this.tv_address = (TextView) findViewById(R.id.tv_address);
+		this.tv_friend = (TextView) findViewById(R.id.tv_friend);
+		this.tv_setting = (TextView) findViewById(R.id.tv_setting);
+
+	}
+
 	private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
 
 		@Override
@@ -65,7 +140,7 @@ public class MoreTabMJSYActivity extends FragmentActivity {
 		}
 	};
 
-	private int size = 5;
+	private int size = 4;
 
 	public void on3(View view) {
 		size = 3;
@@ -124,6 +199,6 @@ public class MoreTabMJSYActivity extends FragmentActivity {
 		public int getItemPosition(Object object) {
 			return FragmentListPageAdapter.POSITION_NONE;
 		}
-	};
+	}
 
 }
